49900b6efe4aceb11c84b41bdfbec5f284fb2261
Artem Stepin <astepin@24h.de>
2016-06-14 14:40:09 +0200

    code style updates

--- tests/unit/Service/ServiceFactoryMock.php
+++ tests/unit/Service/ServiceFactoryMock.php
@@ -26,7 +26,7 @@ class ServiceFactoryMock implements FactoryInterface
      *     creating a service.
      * @throws ContainerException if any other error occurs
      */
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = NULL)
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
         return true;
     }
